bf96765873842eae57eac6017e8ab2b217893a19
Jonah Branch <jonah.branch99@gmail.com>
2015-12-23 23:27:41 -0600

    it generates a room

--- main.cpp
+++ main.cpp
@@ -5,11 +5,21 @@
 const int minimum_size = 5;
 const int maximum_size = 20;
 
-
-
-int generateMapArray(int width, int length)
+int** generateMapArray(int width, int length)
 {
+	int** array; 
+	array = new int*[width];
+	for(int i = 0; i < width; i++)
+		array[i] = new int[length];
+	
+	for(int i = 0; i < width; i++)
+		for(int j = 0; j < length; j++)
+			if(i == 0 || i == width-1 || j == 0 || j == length-1) {
+				array[i][j] = 1;
+			} else
+				array[i][j] = 0;
 
+	return array;
 }
 
 int getSize()
@@ -21,9 +31,24 @@ int main()
 {
         srand(time(NULL));
 
-	int map [22] [22]; // Create largest size map possible
-	std::cout << "Hey, We are alive.\n";
-	const int width = getSize();
-	const int length = getSize();
-	std::cout << "The length is " << length << ".\n";
+	int width = getSize();
+	int length = getSize();
+
+	int** map = generateMapArray(width,length);
+
+	for(int i = 0; i < width; i++) {
+		std::cout << "\n";
+		for(int j = 0; j < length; j++) {
+			if(map[i][j] == 0) {
+				std::cout << " ";
+			} else if(map[i][j] == 1)
+				std::cout << "#";
+		}
+	}
+
+	std::cout << "\n" << "\n";
+
+	for(int i = 0; i < width; i++)
+		delete[] map[i];
+	delete[] map;
 }
